3db25e48d72e57c7ff8c65e119db1c3f9e5176f5
Sita Thomas <sita.thomas@gmail.com>
2020-06-08 21:18:05 -0600

    Add post fields to be extracted - does not include ones from Selenium

--- ynab/ynab/spiders/ynab_spider.py
+++ ynab/ynab/spiders/ynab_spider.py
@@ -7,15 +7,62 @@ class YnabSpider(Spider):
   start_urls = ['https://support.youneedabudget.com/topics?sort=no_sort']
 
   def parse(self, response):
-    # Find the total number of pages in the result so that we can decide how many pages to scrape next
-    page_count = int(response.xpath('//div[@id="pageNav"]/a[5]/text()').extract_first())
-    # List comprehension to construct all the urls
-    page_urls = ['https://support.youneedabudget.com/topics?pg={}&sort=no_sort'.format(x) for x in range(1,page_count)]
-    # Yield the requests to different search result urls, 
-    # using parse_result_page function to parse the response.
+    page_count = int(response.xpath(
+      '//div[@id="pageNav"]/a[5]/text()').extract_first())
+
+    page_urls = ['https://support.youneedabudget.com/topics?pg={}&sort=no_sort'
+      .format(x) for x in range(1,page_count)]
+
     for url in page_urls:
       yield Request(url=url, callback=self.parse_posts_list)
 
   def parse_posts_list(self, response):
-    post_urls = response.xpath('//a[@class="topic-summary__text-link -topic"]/@href').extract()
-    print(len(post_urls))
+    post_urls = response.xpath(
+      '//a[@class="topic-summary__text-link -topic"]/@href').extract()
+
+    for url in ['https://support.youneedabudget.com{}'
+      .format(x) for x in post_urls]:
+        yield Request(url=url, callback=self.parse_post_page)
+
+  def parse_post_page(self, response):
+    title = response.xpath('//h1[@class="topic__title"]/text()').extract_first()
+    
+    user = response.xpath(
+      '//li[@class="topic-meta__item topic-meta__handle at-handle"]/text()'
+    ).extract_first()
+    
+    posted = "" # NEED FROM SELENIUM SCRIPT
+    
+    category = response.xpath(
+      '//a[@class="site-breadcrumb__link -category"][2]/text()'
+    ).extract_first()
+    
+    replies = response.xpath(
+      '//span[@class="panel__value replyCount"]/text()'
+    ).extract_first()
+    
+    views = response.xpath(
+      '//div[@class="panel panel-stats"]/ul/li[4]/span[1]/text()'
+    ).extract_first()
+    
+    last_active = "" # NEED FROM SELENIUM SCRIPT
+    
+    text = response.xpath(
+      '//div[@class="cfa topic__text formatted"]/p/text()'
+    ).extract()
+
+    text = [i.lstrip() for i in text]
+    text = ''.join(text).strip()
+    text = text.replace(u'\xa0', u'')
+
+    item = YnabItem()
+    
+    item["title"] = title
+    item["user"] = user
+    item["posted"] = posted
+    item["category"] = category
+    item["replies"] = replies
+    item["views"] = views
+    item["last_active"] = last_active
+    item["text"] = text
+    yield item
