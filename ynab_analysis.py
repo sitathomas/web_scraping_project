831275eecaa3e15c4225484a58a91c8854055e1c
Sita Thomas <sita.thomas@gmail.com>
2020-06-21 18:41:55 -0600

    Expand basic stats analysis - add stats by category

--- ynab_analysis.py
+++ ynab_analysis.py
@@ -11,64 +11,73 @@ from nltk.probability import FreqDist
 from wordcloud import WordCloud
 from matplotlib import pyplot as plt
 
-
 plt.style.use('ggplot')
 pd.set_option('display.max_columns', None)
 
 ynab = pd.read_csv("ynab/ynab.csv")
 print()
-print("Columns:\n", ynab.columns.tolist())
+
+print("YNAB Columns:\n", ynab.columns.tolist())
 print()
+
 print("Number of posts:", len(ynab))
 print("Number of unique users:", len(set(ynab.user)))
+print("Average posts per user:", round(len(ynab) / len(set(ynab.user)), 2))
 user_group = pd.DataFrame(ynab.groupby("user").size()).sort_values(0, ascending=False)
 user_group["user"] = user_group.index
 user_group.index = range(1, len(user_group)+1)
 user_group.columns = ["posts", "user"]
-user_group["percent"] = round(user_group["posts"] / 7142 * 100, 2)
-user_group = user_group[["user", "posts", "percent"]]
+user_group["ratio"] = round(user_group["posts"] / len(ynab), 2)
+user_group = user_group[["user", "posts", "ratio"]]
 print("Top 10 Users:\n", user_group[:10])
 print()
+
 by_date = ynab.sort_values('posted', ascending=False)
 print("Newest post date:", by_date.posted.iloc[0])
 print("Oldest post date:", by_date.posted.iloc[-1])
 print()
-def top_stats(field):
-  top_post = ynab.sort_values(field, ascending=False)
-  print("Most {}:".format(field), top_post[field].iloc[0], "\nTitle: {}".format(top_post.title.iloc[0]))
-top_stats("likes")
-top_stats("replies")
-top_stats("views")
-top_stats("following")
+
+def build_cat_stats(field):
+  return ynab.groupby("category")[field].sum()
+category_stats = pd.concat([ynab.groupby("category").category.count(), build_cat_stats("likes"), build_cat_stats("replies"), build_cat_stats("views"), build_cat_stats("following")], axis=1)
+category_stats.columns.values[0] = "posts"
 print()
+
+def build_cat_ratios(field):
+  category_stats["{}_ratio".format(field)] = 0
+  new_cells = []
+  for cell in category_stats[field]:
+    new_cells.append(round(cell / category_stats[field].sum(), 2))
+  category_stats["{}_ratio".format(field[0][0])] = new_cells
+build_cat_ratios("posts")
+build_cat_ratios("likes")
+build_cat_ratios("replies")
+build_cat_ratios("views")
+build_cat_ratios("following")
+category_stats = category_stats[["posts", "p_ratio", "likes", "l_ratio", "replies", "r_ratio", "views", "v_ratio", "following", "f_ratio"]]
+print(category_stats)
+print()
+
 def avg_stats(field):
   print("Average {}:".format(field), round(ynab.sort_values(field, ascending=False)[field].mean(), 2))
 avg_stats("likes")
 avg_stats("replies")
 avg_stats("views")
 avg_stats("following")
+print()
+
+def top_stats(field):
+  top_post = ynab.sort_values(field, ascending=False)
+  print("Most {}:".format(field), top_post[field].iloc[0], "\n", top_post.iloc[0], "\n")
+top_stats("likes")
+top_stats("replies")
+top_stats("views")
+top_stats("following")
+print()
 
 
 
-
-
-
-# def category_stats(field):
-#   cat_group = ynab.groupby("category")
-#   for key, values in cat_group:
-#     print("Number of {} per {}:".format(field, key), sum(values[field]))
-# category_stats("likes")
-# category_stats("replies")
-# category_stats("views")
-# category_stats("following")
-# # cat_group = ynab.groupby("category")
-# # for key, values in cat_group:
-# #   print(sum(values.likes))
-# print("\nNumber of posts per category:\n", ynab.groupby("category").category.count())
-
-
-
-### NLP PRE-PROCESSING ###
+# ### NLP PRE-PROCESSING ###
 # ynab.text = ynab.text.str.lower()
 # ynab.text = ynab.text.apply(lambda x: re.sub('[^\w\s]', ' ', x))
 # ynab.text = ynab.text.apply(lambda x: " ".join(word for word in x.split() if word not in stop_words))
@@ -84,13 +93,13 @@ avg_stats("following")
 
 
 
-### N GRAMS###
+# ### N GRAMS###
 # def find_ngrams(input_list, n):
 #     return list(zip(*[input_list[i:] for i in range(n)]))
 # text_ngrams = ynab.text.apply(lambda x: find_ngrams(x.split(), 3))
 # print(text_ngrams[0])
 
-### WORD CLOUD FOR ALL CATEGORIES ###
+# ### WORD CLOUD FOR ALL CATEGORIES ###
 # wc = WordCloud(background_color="white", max_words=100, width=800, height=400)
 # wc.generate(' '.join(ynab.text))
 # plt.figure(figsize=(12, 6))
@@ -98,7 +107,7 @@ avg_stats("following")
 # plt.axis("off")
 # plt.show()
 
-### WORD CLOUDS FOR EACH CATEGORY ###
+# ### WORD CLOUDS FOR EACH CATEGORY ###
 # accts_trans = ynab.groupby("category")
 # for item in accts_trans:
 #   wc = WordCloud(background_color="white", max_words=100, width=800, height=400)
